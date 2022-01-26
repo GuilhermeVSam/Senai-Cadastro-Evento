65a770f74cbccf4276baaf31ea1517616e8b06c0
Guilherme <guiwojtysiak053@hotmail.com>
2022-01-26 17:31:53 -0300

    Código
    Update no Código Base Aplicando Sistema de Listagem dos Participantes

--- Senai-Cadastro-Evento.js
+++ Senai-Cadastro-Evento.js
@@ -1,5 +1,5 @@
 const DataAtual = new Date();
-var DataEvento = new Date(2022,05,25);
+let DataEvento = new Date(2022,05,25);
 
 if (DataEvento > DataAtual) {                               //Verificar se a data do evento e posterior a data atual.
     console.log ("Data do Evento Valida!");
@@ -8,7 +8,7 @@ if (DataEvento > DataAtual) {                               //Verificar se a dat
 }
 
 const IdadePermitida = Number (18);
-var IdadeDoUsuario = Number (24);
+let IdadeDoUsuario = Number (24);
 
 if (IdadeDoUsuario >= IdadePermitida) {                     //Verificar se o participante possui a idade minima para comparecer ao evento.
     console.log ("Aguarde...");
@@ -17,17 +17,23 @@ if (IdadeDoUsuario >= IdadePermitida) {                     //Verificar se o par
 }
 
 
-var Participantes = Number (95);                           //Declaracao da variavel Participantes.
+let Participantes = ['Marcos', 'Joao', 'Felipe', 'Maria', 'Pedro'];
+let QuantidadeParticipantes = Participantes.length;
+let inicio = 0
 
-if (Participantes < 100) {                                  //Verificar se o evento ainda possui capacidade para novos participantes.
-    Participantes++;
+if (QuantidadeParticipantes < 100) {                                  //Verificar se o evento ainda possui capacidade para novos participantes.
+    console.log ("Os participantes do evento são: ");
+    do {
+        console.log(Participantes[inicio]);
+        inicio++;
+    } while (inicio < QuantidadeParticipantes);
 }
 else{
     Participantes++;
     console.log("Desculpe, o Evento Atingiu a Capacidade Maxima de Participantes");
 }
 
-if(DataEvento > DataAtual && IdadeDoUsuario >= IdadePermitida && Participantes <= 100){         //Verificar se todos os requisitos foram atingidos + confirmacao da criacao do evento.
+if(DataEvento > DataAtual && IdadeDoUsuario >= IdadePermitida && QuantidadeParticipantes <= 100){         //Verificar se todos os requisitos foram atingidos + confirmacao da criacao do evento.
     console.log("Seu Evento Foi Criado com Sucesso!");
 } else {
     console.log("Erro. O Evento Nao Pode ser Criado.");
